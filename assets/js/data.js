7069abffde87ef431c1bd5fb304265855deb628a
benja253 <benja48.9@gmail.com>
2024-08-12 11:55:29 -0400

    tercer intento

--- assets/js/data.js
+++ assets/js/data.js
@@ -11,5 +11,3 @@ const cambio = {
 const palabras = arrWordle.map(word => {
   return word.split('').map(e => cambio[e] || e).join('')
 }).toSorted().filter(e => e.length === 5)
-
-console.log([...new Set(palabras)])
